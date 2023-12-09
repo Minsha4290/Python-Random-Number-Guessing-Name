4c070d66b00dae6c58361939672a3090f41b83d1
Minsha4290 <139846117+Minsha4290@users.noreply.github.com>
2023-12-09 19:41:19 +0530

    python_random_number_guesser.py

--- python_random_number_guesser.py
+++ python_random_number_guesser.py
@@ -1,23 +1,32 @@
 import random
 
-ANSWER = random.randint(0,10)
-TRIES = 3
+def play_guessing_game():
+    while True:
+        ANSWER = random.randint(0, 10)
+        TRIES = 3
 
-print('Welcome! I have chosen a random number between 0 and 10.')
-print('You have', TRIES, 'tries to guess the number.')
+        print('\nWelcome! I have chosen a random number between 0 and 10.')
+        print('You have', TRIES, 'tries to guess the number.')
 
-for tries in range(TRIES):
-    guess = int(input('Enter your guess: '))
-    if guess == ANSWER:
-        print('You guessed correctly! You win!')
-        break
-    elif guess < ANSWER:
-        print('Sorry, your answer is too low!')
-        if tries < TRIES-1:
-            print('You have', TRIES - tries - 1, 'tries left')
-    elif guess > ANSWER:
-        print('Sorry, your answer is too high!')
-        if tries < TRIES-1:
-            print('You have', TRIES - tries - 1, 'tries left.')
-else:
-    print('Sorry you lost...the number was',ANSWER)
+        for tries in range(TRIES):
+            guess = int(input('Enter your guess: '))
+            if guess == ANSWER:
+                print('You guessed correctly! You win!')
+                break
+            elif guess < ANSWER:
+                print('Sorry, your answer is too low!')
+                if tries < TRIES - 1:
+                    print('You have', TRIES - tries - 1, 'tries left')
+            elif guess > ANSWER:
+                print('Sorry, your answer is too high!')
+                if tries < TRIES - 1:
+                    print('You have', TRIES - tries - 1, 'tries left.')
+        else:
+            print('Sorry you lost...the number was', ANSWER)
+
+        play_again = input('Do you want to play again? (yes/no): ').lower()
+        if play_again != 'yes':
+            print('Goodbye!')
+            break
+
+play_guessing_game()
